f529a4843dbfca37761cb5726dc55c6fad87df54
NoahVlncrt <noah.vaillancourt@gmail.com>
2017-11-28 14:15:50 -0500

    initial commit

---
+++ imports/ui/components/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => (
+    <div>
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Navbar</a>
+        </nav>
+    </div>
+)
+
+export default Navigation
